707d97a2b9b95a240b75d221ffcfb0063a5b6145
Damon <435801541@qq.com>
2024-03-03 15:05:25 +0800

    feat: wheel collider

---
+++ PhysicsEngine/Assets/Vehicle/Scripts/Car.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [Tooltip("车前左侧车轮碰撞体")]
+    public WheelCollider frontLeftWheelCollider;
+    [Tooltip("车前右侧车轮碰撞体")]
+    public WheelCollider frontRightWheelCollider;
+    [Tooltip("虚拟摇杆")]
+    public EasyJoystick myJoystick;
+    [Tooltip("最大力矩")]
+    public float maxTorque = 500;
+    [Tooltip("最大旋转角")]
+    public float maxAngle = 20;
+    private void Start()
+    {
+        // 调整赛车刚体重心
+        GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -0.8f, 0);
+    }
+    private void FixedUpdate()
+    {
+        // 控制力矩
+        frontLeftWheelCollider.motorTorque = maxTorque * myJoystick.JoystickTouch.y;
+        // 控制旋转角
+        frontLeftWheelCollider.steerAngle = maxAngle * myJoystick.JoystickTouch.x;
+        frontRightWheelCollider.motorTorque = maxTorque * myJoystick.JoystickTouch.y;
+        frontRightWheelCollider.steerAngle = maxAngle * myJoystick.JoystickTouch.x;
+    }
+}
---
+++ PhysicsEngine/Assets/Vehicle/Scripts/Wheel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    [Tooltip("车轮碰撞体")]
+    public WheelCollider wheelCollider;
+    [Tooltip("车轮滚动角")]
+    public float cirValue = 0;
+
+    void Update()
+    {
+        transform.rotation = wheelCollider.transform.rotation * Quaternion.Euler(cirValue, wheelCollider.steerAngle, 0);
+        cirValue += wheelCollider.rpm * 360 / 60 * Time.deltaTime;
+    }
+}
